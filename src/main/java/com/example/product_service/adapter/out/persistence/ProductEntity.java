623211b20d9346d023cf4ae85d35ab9c63fd1c7f
Jun <jundev21@gmail.com>
2026-08-27 10:38:57 +0900

    [feat] spring jpa 설정

--- src/main/java/com/example/product_service/adapter/out/persistence/ProductEntity.java
+++ src/main/java/com/example/product_service/adapter/out/persistence/ProductEntity.java
@@ -1,13 +1,14 @@
 package com.example.product_service.adapter.out.persistence;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "products")
+@Table(name="products")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
